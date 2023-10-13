5bbe61eb68a7bc851eb11d18b359e0e0368844ca
yealingt@gmail.com <yealingt@gmail.com>
2023-10-13 08:14:23 +0000

    Created Dashboard.Coalition.Cadet and Dashboard.Coalition; Edited settings and Dashboard; Deleted Dashboard.Cadet

--- client_code/Dashboard/Coalition/__init__.py
+++ client_code/Dashboard/Coalition/__init__.py
@@ -1,13 +1,14 @@
-from ._anvil_designer import DashboardTemplate
+from ._anvil_designer import CoalitionTemplate
 from anvil import *
 import anvil.server
 import anvil.users
-from Cadet import Cadet
 
-class Dashboard(DashboardTemplate):
+class Coalition(CoalitionTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    
+    self.label_1.text = 'Coalition'
+    self.repeating_panel_1.item_template = Cadet
+    self.repeating_panel_1.items = anvil.server.call('getStudentObject')
 
     # Any code you write here will run before the form opens.
