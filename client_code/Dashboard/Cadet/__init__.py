1bc9ed913b56672b0101f5cee0d121328a5ef11c
yealingt@gmail.com <yealingt@gmail.com>
2023-10-13 10:56:41 +0000

    Colour theme and whacky margins

--- client_code/Dashboard/Cadet/__init__.py
+++ client_code/Dashboard/Cadet/__init__.py
@@ -7,7 +7,12 @@ class Cadet(CadetTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.label_1.text = self.item['login'] + ':' + self.item['displayname']
-    self.image_1.source = self.item['image']['versions']['small']
+    self.label_1.text = self.item['displayname']
+    self.label_2.text = self.item['login']
+    student_image = self.item['image']['versions']['small']
+    if student_image is None:
+      student_image = URLMedia('_/theme/whacky-too.png')
+    self.image_1.source = student_image
+    self.image_1.display_mode = 'original_size'
 
     # Any code you write here will run before the form opens.
