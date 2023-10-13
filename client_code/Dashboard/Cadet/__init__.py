c5888f2ccc0786c965c49983a3560daf3874b894
yealingt@gmail.com <yealingt@gmail.com>
2023-10-13 07:21:26 +0000

    Edited Dashboard.Cadet

--- client_code/Dashboard/Cadet/__init__.py
+++ client_code/Dashboard/Cadet/__init__.py
@@ -7,7 +7,7 @@ class Cadet(CadetTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.label_1.text = self.item['login']
+    self.label_1.text = self.item['login'] + ':' + self.item['displayname']
     self.image_1.source = self.item['image']['versions']['medium']
 
     # Any code you write here will run before the form opens.
