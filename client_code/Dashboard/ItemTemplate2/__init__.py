9ebf8deb48a3b7d6cd85d402c1ce146081a7a355
yealingt@gmail.com <yealingt@gmail.com>
2023-10-13 08:23:23 +0000

    Created Dashboard.ItemTemplate2; Edited Dashboard and Dashboard.Coalition

--- client_code/Dashboard/ItemTemplate2/__init__.py
+++ client_code/Dashboard/ItemTemplate2/__init__.py
@@ -1,15 +1,11 @@
-from ._anvil_designer import CoalitionTemplate
+from ._anvil_designer import ItemTemplate2Template
 from anvil import *
 import anvil.server
 import anvil.users
-from .Cadet import Cadet
 
-class Coalition(CoalitionTemplate):
+class ItemTemplate2(ItemTemplate2Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.label_1.text = 'Coalition'
-    self.repeating_panel_1.item_template = Cadet
-    self.repeating_panel_1.items = items[set]
 
     # Any code you write here will run before the form opens.
--- client_code/Dashboard/__init__.py
+++ client_code/Dashboard/__init__.py
@@ -2,12 +2,13 @@ from ._anvil_designer import DashboardTemplate
 from anvil import *
 import anvil.server
 import anvil.users
-from Cadet import Cadet
+from .Coalition import Coalition
 
 class Dashboard(DashboardTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    
+    self.repeating_panel_1.item_template = Coalition
+    self.repeating_panel_1.items = anvil.server.call('getStudentObject')
 
     # Any code you write here will run before the form opens.
