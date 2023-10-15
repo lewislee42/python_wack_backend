8a1163620fed6287c96960ef261f59a367ad58ad
lewislee313@gmail.com <lewislee313@gmail.com>
2023-10-15 04:34:08 +0000

    Edited Dashboard

--- client_code/Dashboard/__init__.py
+++ client_code/Dashboard/__init__.py
@@ -8,7 +8,9 @@ class Dashboard(DashboardTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.repeating_panel_1.item_template = Coalition
-    self.repeating_panel_1.items = anvil.server.call('getStudentObject')
+    val = anvil.server.call('getStudentByLogin', 'lewlee')
+    print(val)
+    # self.repeating_panel_1.item_template = Coalition
+    # self.repeating_panel_1.items = anvil.server.call('getStudentObject')
 
     # Any code you write here will run before the form opens.
