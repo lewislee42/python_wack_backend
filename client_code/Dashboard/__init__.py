2ac7c8629206ef453179070ffc3912f5535530b0
yealingt@gmail.com <yealingt@gmail.com>
2023-10-15 03:07:20 +0000

    Edited theme.css, Dashboard and theme/parameters.yaml

--- client_code/Dashboard/__init__.py
+++ client_code/Dashboard/__init__.py
@@ -20,3 +20,21 @@ class Dashboard(DashboardTemplate):
     
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.repeating_panel_4.visible = False if self.repeating_panel_4.visible else True
+
+  def button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.repeating_panel_2.visible = False if self.repeating_panel_2.visible else True
+
+  def button_3_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.repeating_panel_1.visible = False if self.repeating_panel_1.visible else True
+
+  def button_4_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.repeating_panel_3.visible = False if self.repeating_panel_3.visible else True
+
+
